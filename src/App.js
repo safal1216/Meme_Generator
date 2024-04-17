40be5c6db810f06f39ae384506d38d8c0d808520
Safal Mehrotra <safalmehrotra1216@gmail.com>
2024-04-17 21:16:13 +0530

    main section is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from "react";
 import Head from "./head";
+import Main from "./main";
 
 export default function App() {
     return (
         <div>
             <Head />
+            <Main />
         </div>
     )
 }
---
+++ src/main.js
@@ -0,0 +1,34 @@
+
+import React from "react";
+
+export default function Main() {
+    return (
+        <main>
+            <form className="form">
+                <div>
+                    <label htmlFor="top-text">Top Text</label>
+                    <input
+                        id="top-text"
+                        type="text"
+                        placeholder="Shut up"
+                        className="form--input"
+                    />
+                </div>
+                <div>
+                    <label>Bottom Text
+                    <input
+                            type="text"
+                            placeholder="and take my money"
+                            className="form--input"
+                        />
+                    </label>
+                </div>
+                <button
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
+    )
+}
